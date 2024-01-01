d0c5549e39964b2019a523c3a14ba094326b896d
Jainulhashmi1 <155364768+Jainulhashmi1@users.noreply.github.com>
2024-01-01 19:02:45 +0530

    Create numeric.py

---
+++ numeric.py
@@ -0,0 +1,30 @@
+print("excercise 2 Introducing the int data type")
+print("python has three numeric types:int,float,complex")
+print(".........................................................................................")
+
+print(" Creating a variable")
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print(".........................................................................................")
+
+print("excercise 3 introduction of float type")
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print(".........................................................................................")
+
+print("excercise 4 Introducing the complex data type")
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+print(".........................................................................................")
+
+print("excercise 5 Introducing the bool data type")
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
