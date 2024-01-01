cfbb326a37a937d2cfaadd2b7341cd6f5e7e275a
Jainulhashmi1 <155364768+Jainulhashmi1@users.noreply.github.com>
2024-01-01 19:31:53 +0530

    Create list_tuple_dictionary.py

---
+++ list_tuple_dictionary.py
@@ -0,0 +1,56 @@
+print("             Working with Lists, Tuples, and Dictionaries               ")
+print(".........................................................................................")
+
+print("Exercise 1: Introducing the list data type")
+
+print("     Defining a list     ")
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(".........................................................................................")
+
+
+print("     Accessing a list by position      ")
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+print(".........................................................................................")
+
+print("     Changing the values in a list      ")
+myFruitList[2] = "orange"
+print(myFruitList)
+print(".........................................................................................")
+
+print("Exercise 2: Introducing the tuple data type")
+
+print("     Defining a tuple      ")
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(".........................................................................................")
+
+print("     Accessing a tuple by position      ")
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+print(".........................................................................................")
+
+print("Exercise 3: Introducing the dictionary data type")
+
+print("     Defining a dictionary      ")
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(".........................................................................................")
+
+print("     Accessing a dictionary by name      ")
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+
+
